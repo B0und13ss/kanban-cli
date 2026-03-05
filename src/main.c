da2b02b48340a2c0e2cf42355b843744e4fe6152
B0und13ss <idougherty0706@gmail.com>
2026-03-05 16:51:14 -0500

    Messed with time

--- src/main.c
+++ src/main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
+
+char* getCurrentHumanTime();
+
+typedef struct {
+  char* title;
+  char* body;
+  char* createTimeHuman;
+  char* completeTimeHuman;
+  time_t createTime;
+  time_t completeTime;
+} task;
 
 int main (int argc, char** args) {
-  printf("This is the beginning!\n");
+
+  char* currentTime = getCurrentHumanTime();
+
+  printf("The time is: %s\n", currentTime);
+
+  free(currentTime);
+
   return EXIT_SUCCESS;
+}
+
+char* getCurrentHumanTime() {
+  char* formatTime = malloc((sizeof(char) * 20));
+  time_t epoch = time(NULL);
+  struct tm* local_time = localtime(&epoch);
+  strftime(formatTime, (sizeof(formatTime) * 20), "%Y-%m-%d %H:%M:%S", local_time);
+
+  return formatTime;
 }
